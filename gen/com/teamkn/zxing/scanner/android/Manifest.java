52ed749f8ad488ff2d4cc81d7caa78b97332ba01
Kaid <kaid@kaid.me>
2012-12-29 23:42:10 +0800

    renamed the package and formatted the code

---
+++ gen/com/teamkn/zxing/scanner/android/Manifest.java
@@ -0,0 +1,5 @@
+package com.teamkn.zxing.scanner.android;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
---
+++ gen/com/teamkn/zxing/scanner/android/R.java
@@ -0,0 +1,5 @@
+package com.teamkn.zxing.scanner.android;
+
+/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
+public final class R {
+}
